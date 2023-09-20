57b7f7c98b69a0d102323879edb7a0caa656ce11
Shion <umaradri@student.21-school.ru>
2023-09-20 21:06:35 +0700

    add constructors and some helpful func

--- src/red_black_tree/red_black_tree.h
+++ src/red_black_tree/red_black_tree.h
@@ -3,33 +3,138 @@
 
 #include <functional>
 
-#include "node.h"
-
 namespace s21 {
+
+enum Color { kRed, kBlack };
 
 template <typename Key, typename Compare = std::less<Key>>
 class RedBlackTree {
  private:
-    struct Iterator;
-    struct IteratorConst;
+  struct Node;
+  struct Iterator;
+  struct IteratorConst;
 
  public:
-    using key_type = Key;
-    using reference = key_type &;
-    using const_reference = const key_type &;
-    using iterator = Iterator;
-    using const_iterator = IteratorConst;
-    using size_type = std::size_t;
+  using key_type = Key;
+  using reference = key_type &;
+  using const_reference = const key_type &;
+  using iterator = Iterator;
+  using const_iterator = IteratorConst;
+  using size_type = std::size_t;
 
-    RedBlackTree();
-    RedBlackTree(const RedBlackTree &other);
-    RedBlackTree(RedBlackTree &&other) noexcept;
-    RedBlackTree &operator=(const RedBlackTree &other);
-    RedBlackTree &operator=(RedBlackTree &&other) noexcept;
-    ~RedBlackTree();
+  RedBlackTree();
+  RedBlackTree(const RedBlackTree &other);
+  RedBlackTree(RedBlackTree &&other) noexcept;
+  RedBlackTree &operator=(const RedBlackTree &other);
+  RedBlackTree &operator=(RedBlackTree &&other) noexcept;
+  ~RedBlackTree();
+
+  void CopyTree(Node *node);
+  Node *CopyNode(const Node *node, Node *parent);
+  void RemoveNode(Node *node);
+  void RemoveTree();
+  Node *GetRoot();
+  void SetRoot(Node *node);
+  void SetHead();
+  size_type GetSize();
+
+  struct Node {
+    Node()
+        : parent(nullptr),
+          left(this),
+          right(this),
+          key(key_type{}),
+          color(kRed) {}
+
+    Node(const key_type &key)
+        : parent(nullptr),
+          left(nullptr),
+          right(nullptr),
+          key(key),
+          color(kRed) {}
+
+    Node(key_type &&key)
+        : parent(nullptr),
+          left(nullptr),
+          right(nullptr),
+          key(std::move(key)),
+          color(kRed) {}
+
+    Node(key_type key, Color color)
+        : parent(nullptr),
+          left(nullptr),
+          right(nullptr),
+          key(key),
+          color(color) {}
+
+    void ToDefault() noexcept {
+      left = nullptr;
+      right = nullptr;
+      parent = nullptr;
+      color = kRed;
+    }
+
+    GetNextNode() const noexcept {
+      Node *node = const_cast<Node *>(this);
+      if (node->color == kRed &&
+          (node->parent == nullptr || node->parent->parent == node)) {
+        node = node->left;
+      } else if (node->right != nullptr) {
+        node = node->right;
+
+        while (node->left != nullptr) {
+          node = node->left;
+        }
+      } else {
+        Node *parent = node->parent;
+
+        while (node == parent->right) {
+          node = parent;
+          parent = parent->parent;
+        }
+        if (node->right != parent) {
+          node = parent;
+        }
+      }
+
+      return node;
+    }
+
+    GetPreviousNode() const noexcept {
+      Node *node = const_cast<Node *>(this);
+
+      if (node->color == kRed &&
+          (node->parent == nullptr || node->parent->parent == node)) {
+        node = node->right;
+      } else if (node->left != nullptr) {
+        node = node->left;
+        while (node->right != nullptr) {
+          node = node->right;
+        }
+      } else {
+        Node *parent = node->parent;
+        while (node == parent->left) {
+          node = parent;
+          parent = parent->parent;
+        }
+        if (node->left != parent) {
+          node = parent;
+        }
+      }
+      return node;
+    }
+
+    Node *parent;
+    Node *left;
+    Node *right;
+    key_type key;
+    Color color;
+  };
 
  private:
-    Node *head_;
+  Node *head;
+  size_type tree_size;
+  Compare cmp;
 };
 
 }  // namespace s21
--- src/red_black_tree/red_black_tree.tpp
+++ src/red_black_tree/red_black_tree.tpp
@@ -3,33 +3,125 @@
 namespace s21 {
 
 template <typename Key, typename Compare>
-RedBlackTree<Key, Compare>::RedBlackTree() {
-
-}
+RedBlackTree<Key, Compare>::RedBlackTree() : head(new Node), tree_size(0) {}
 
 template <typename Key, typename Compare>
 RedBlackTree<Key, Compare>::RedBlackTree(const RedBlackTree &other) {
-
+    if (other.GetSize() != 0) {
+        CopyTree(other);
+    }
 }
 
 template <typename Key, typename Compare>
 RedBlackTree<Key, Compare>::RedBlackTree(RedBlackTree &&other) noexcept {
-
+    // Swap(other);
 }
 
 template <typename Key, typename Compare>
-typename RedBlackTree<Key, Compare>::RedBlackTree & RedBlackTree<Key, Compare>::operator=(const RedBlackTree &other) {
+typename RedBlackTree<Key, Compare>::RedBlackTree &
+RedBlackTree<Key, Compare>::operator=(const RedBlackTree &other) {
+    if (this != &other) {
+        if (other.GetSize() != 0) {
+            CopyTree(other);
+        } else {
+            RemoveTree();
+        }
+    }
 
+    return *this;
 }
 
 template <typename Key, typename Compare>
-typename RedBlackTree<Key, Compare>::RedBlackTree & RedBlackTree<Key, Compare>::operator=(RedBlackTree &&other) noexcept {
-
+typename RedBlackTree<Key, Compare>::RedBlackTree &
+RedBlackTree<Key, Compare>::operator=(RedBlackTree &&other) noexcept {
+    RemoveTree();
+    Swap(other);
+    return *this;
 }
 
 template <typename Key, typename Compare>
 RedBlackTree<Key, Compare>::~RedBlackTree() {
+    RemoveTree();
+    delete head;
+    head = nullptr;
+}
 
+template <typename Key, typename Compare>
+void RedBlackTree<Key, Compare>::CopyTree(Node *node) {
+    Node *copy = CopyNode(node.Root(), node.Root()->parent);
+
+    RemoveTree();
+    SetRoot(copy);
+    Root()->parent = head;
+    tree_size = node.tree_size;
+    cmp = node.cmp;
+}
+
+template <typename Key, typename Compare>
+typename RedBlackTree<Key, Compare>::Node *RedBlackTree<Key, Compare>::CopyNode(
+    const Node *node, Node *parent) {
+  Node *copy = new Node{node->key, node->color};
+
+  try {
+    if (node->left) {
+      copy->left = CopyNode(node->left, copy)
+    }
+
+    if (node->right) {
+      copy->right = CopyNode(node->rigth, copy)
+    }
+  } catch {
+    RemoveNode(copy);
+    throw;
+  }
+
+  copy->parent = parent;
+  return copy;
+}
+
+template <typename Key, typename Compare>
+void RedBlackTree<Key, Compare>::RemoveNode(Node *node) {
+  if (!node) {
+    return;
+  }
+
+  RemoveNode(node->left);
+  RemoveNode(node->rigth);
+  delete node;
+}
+
+template <typename Key, typename Compare>
+void RedBlackTree<Key, Compare>::RemoveTree() {
+  RemoveNode(GetRoot());
+  SetHead();
+  tree_size = 0;
+}
+
+template <typename Key, typename Compare>
+typename RedBlackTree<Key, Compare>::Node *
+RedBlackTree<Key, Compare>::GetRoot() {
+  return head->parent;
+}
+
+template <typename Key, typename Compare>
+void RedBlackTree<Key, Compare>::SetHead() {
+  SetRoot(nullptr);
+  // // Т.к. элементов нет, то самый маленький (самый левый) элемент будет
+  // // указывать на голову
+  // MostLeft() = head_;
+  // // Т.к. элементов нет, то самый большой (самый правый) элемент будет
+  // // указывать на голову
+  // MostRight() = head_;
+}
+
+template <typename Key, typename Compare>
+void RedBlackTree<Key, Compare>::SetRoot(Node *node) {
+  head->parent = node;
+}
+
+template <typename Key, typename Compare>
+size_type RedBlackTree<Key, Compare>::GetSize() {
+  return tree_size;
 }
 
 }  // namespace s21
